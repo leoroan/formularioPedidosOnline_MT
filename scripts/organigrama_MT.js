c7f95e6ab6237d7b26bda91049345bc4d3043e2b
Leandro <leoroan@gmail.com>
2023-05-17 23:07:12 -0300

    organigrama mt creado

---
+++ scripts/organigrama_MT.js
@@ -0,0 +1,392 @@
+let MinisterioDeTransporte = {
+
+  subsecretarias: {
+
+    subsecretariaTransporteTerrestre: {
+      nombre: "Subsecretaría de transporte terrestre",
+      direcciones: {
+
+        oficinaPrivada: {
+          nombre: "Oficina Privada de la Subsecretaría de transporte terrestre"
+        },
+
+        direccionFiscalizacionTransporte: {
+          nombre: "Dirección Provincial de fiscalización del transporte",
+          direcciones: {
+            direccionFiscalizacionTransporte: {
+              nombre: "Oficina privada de la misma direccion",
+            },
+            DirecciónControltecnico: {
+              nombre: "Dirección de control técnico",
+            },
+            DirecciónReclamosSolicitudes: {
+              nombre: "Dirección de reclamos y solicitudes",
+            }
+          },
+        },
+
+        direccionTransportePasajeros: {
+          nombre: "Dirección provincial de transporte de pasajeros",
+          direcciones: {
+            direccionTransportePasajeros: {
+              nombre: "Oficina privada de la misma dirección",
+            },
+            DirecciónTransporteAutomotorPasajeros: {
+              nombre: "Dirección de Transporte Automotor de Pasajeros",
+            },
+            direccionPermisosHabilitacionesPasajeros: {
+              nombre: "Dirección de Permisos y Habilitaciones de Transporte de Pasajeros",
+            },
+            direccionTransporteFerroviarioPasajeros: {
+              nombre: "Dirección de transporte ferroviario de pasajeros",
+            }
+          },
+        },
+
+        direccionTransporteCargas: {
+          nombre: "Dirección provincial de transporte de cargas",
+          direcciones: {
+            direccionTransporteCargas: {
+              nombre: "Oficina privada de la misma dirección",
+            },
+            direccionPermisosHabilitacionesCargas: {
+              nombre: "Dirección de permisos y habilitaciones de transporte de cargas",
+            },
+            direccionTransporteAutomotorCargas: {
+              nombre: "Dirección de transporte automotor de cargas",
+            },
+            direccionTransporteFerroviarioCargas: {
+              nombre: "Dirección de transporte ferroviario de cargas",
+            }
+          }
+        },
+
+        direccionAptitudConductores: {
+          nombre: "Direccion provincial de aptitud psicofisica para conductores del transporte publico"
+        },
+
+        direccionPoliticasTransporteSostenible: {
+          nombre: "Dirección provincial de políticas de transporte sostenible",
+          direcciones: {
+            direccionPoliticasTransporteSostenible: {
+              nombre: "Oficina privada de la misma dirección",
+            },
+            direccionDesarrolloSostenibleTransporte: {
+              nombre: "Dirección de desarrollo sostenible del transporte",
+            },
+            direccionProyectosEstrategicos: {
+              nombre: "Dirección de proyectos estratégicos",
+            }
+          }
+        },
+
+        direccionVerificacionTecnicaVehicular: {
+          nombre: "Dirección provincial de verificación técnica vehicular",
+          direcciones: {
+            direccionVerificacionTecnicaVehicular: {
+              nombre: "Oficina privada de la misma dirección",
+            },
+            direccionApoyoLegalTecnicoAdmin: {
+              nombre: "Dirección de apoyo legal y técnico administrativo",
+            },
+            direccionPlanificacionCoordServicio: {
+              nombre: "Dirección de planificación y coordinación del servicio",
+            },
+            direccionFiscalizacionControl: {
+              nombre: "Dirección de fiscalización y control",
+            }
+          }
+        }
+
+      }
+    },
+
+    subsecretariaPoliticaSeguridadVial: {
+      nombre: "Subsecretaría de política y seguridad vial",
+      direcciones: {
+
+        oficinaPrivada: {
+          nombre: "Oficina Privada de Subsecretaría de política y seguridad vial"
+        },
+
+        direccionLicenciasConducirAntecedentesTransito: {
+          nombre: "Dirección provincial de licencias de conducir y antecedentes de tránsito",
+          direcciones: {
+            direccionLicenciasConducirAntecedentesTransito: {
+              nombre: "Oficina privada de la misma dirección"
+            },
+            direccionLicenciasConducir: {
+              nombre: "Dirección de licencias de conducir"
+            },
+            direccionAntecedentesInhabilitados: {
+              nombre: "Dirección de antecedentes e inhabilitados"
+            }
+          }
+        },
+
+        direccionObservatorioVial: {
+          nombre: "Dirección provincial de observatorio vial",
+          direcciones: {
+            direccionObservatorioVial: {
+              nombre: "Oficina privada de la misma dirección"
+            },
+            direccionEstadisticaVial: {
+              nombre: "Dirección de estadística vial"
+            },
+            direccionCapacitacionEnlaceComunitario: {
+              nombre: "Dirección de capacitación y enlace comunitario"
+            }
+          }
+        },
+
+        direccionFiscalizacionControlTransitoSeguridadVial: {
+          nombre: "Dirección provincial de fiscalización y control del tránsito y la seguridad vial",
+          direcciones: {
+            direccionFiscalizacionControlTransitoSeguridadVial: {
+              nombre: "Oficina privada de la misma dirección",
+            },
+            direccionFiscalizacionControlTransito: {
+              nombre: "Dirección de fiscalización y control del tránsito",
+            },
+            direccionSeguridadVial: {
+              nombre: "Dirección de seguridad vial",
+            }
+          }
+        },
+
+        direccionApoyoCoordTecnicoAdministrativa: {
+          nombre: "Dirección provincial de apoyo y coordinación técnico administrativa",
+          direcciones: {
+            juzgadosAdministrativos: {
+              nombre: "Juzgados administrativos de infracciones de tránsito provincial (I a XIII)"
+            },
+            direccionApoyoAdministrativo: {
+              nombre: "Dirección de apoyo administrativo"
+            },
+            direccionCoordinacionJusticiaAdministrativa: {
+              nombre: "Dirección de coordinación y organización de la justicia administrativa de infracciones de tránsito provincial"
+            },
+            direccionInformaticaTecnologia: {
+              nombre: "Dirección de Informática y tecnología"
+            }
+          }
+        }
+      }
+    }
+  },
+
+  subsecretariaTecnicaAdministrativaLegal: {
+    nombre: "Subsecretaría técnica administrativa y legal",
+    direcciones: {
+      oficinaPrivada: {
+        nombre: "Oficina Privada de Subsecretaría técnica administrativa y legal"
+      },
+      direccionGeneralAdministracion: {
+        nombre: "Dirección general de administración",
+        direcciones: {
+          direccionGeneralAdministracion: {
+            nombre: "Oficina privada de la misma dirección"
+          },
+          direccionContabilidad: {
+            nombre: "Dirección de contabilidad"
+          },
+          direccionPresupuesto: {
+            nombre: "Dirección de presupuesto"
+          },
+          direccionComprasContratacionesSuministros: {
+            nombre: "Dirección de compras, contrataciones y suministros"
+          },
+          direccionTesoreria: {
+            nombre: "Dirección de tesorería"
+          },
+          direccionDelegadaPersonal: {
+            nombre: "Dirección delegada de la dirección provincial de personal"
+          },
+          direccionTecnologiaInformacion: {
+            nombre: "Dirección de tecnología de la información"
+          },
+          direccionServiciosAuxiliares: {
+            nombre: "Dirección de servicios auxiliares"
+          },
+          direccionServiciosTecnicos: {
+            nombre: "Dirección de servicios técnicos"
+          }
+        }
+      },
+
+      direccionGeneralTecnicaLegal: {
+        nombre: "Dirección general Técnica y legal",
+        direcciones: {
+          direccionGeneralTecnicaLegal: {
+            nombre: "Oficina privada de la misma dirección"
+          },
+          direccionTecnicoAdministrativa: {
+            nombre: "Dirección técnico administrativa"
+          },
+          direccionCoordinacionJuridica: {
+            nombre: "Dirección de coordinación jurídica"
+          },
+          direccionGestionMultas: {
+            nombre: "Dirección de gestión de multas por infracciones de tránsito y transporte"
+          }
+        }
+      }
+    }
+  },
+
+  subsecretariaTransporteAereoFluvial: {
+    nombre: "Subsecretaría de transporte aéreo y fluvial",
+    direcciones: {
+      oficinaPrivada: {
+        nombre: "Oficina Privada de Subsecretaría de transporte aéreo y fluvial",
+      },
+
+      direccionContralorTransporteFluvial: {
+        nombre: "Dirección provincial de contralor del transporte fluvial de pasajeros",
+        direcciones: {
+          direccionContralorTransporteFluvial: {
+            nombre: "Oficina privada de la misma dirección"
+          },
+          direccionOperacionesTransporteFluvial: {
+            nombre: "Dirección de operaciones de transporte fluvial de pasajeros"
+          },
+          direccionFiscalizacionTransporteFluvial: {
+            nombre: "Dirección de fiscalización del transporte fluvial de pasajeros"
+          }
+        }
+      },
+
+      direccionContralorTransporteAereo: {
+        nombre: "Dirección provincial de contralor del transporte aéreo",
+        direcciones: {
+          direccionContralorTransporteAereo: {
+            nombre: "Oficina privada de la misma dirección"
+          },
+          direccionOperacionesTransporteAereo: {
+            nombre: "Dirección de operaciones del transporte aéreo"
+          },
+          direccionFiscalizacionAeroportuaria: {
+            nombre: "Dirección de fiscalización aeroportuaria"
+          }
+        }
+      },
+
+      direccionPromocionCoordinacionTransporteAereoFluvial: {
+        nombre: "Dirección provincial de promoción y coordinación del transporte aéreo y fluvial",
+        direcciones: {
+          direccionPromocionCoordinacionTransporteAereoFluvial: {
+            nombre: "Oficina privada de la misma dirección"
+          },
+          direccionEstrategiasPortuariasAeroportuarias: {
+            nombre: "Dirección de estrategias portuarias y aeroportuarias"
+          },
+          direccionPromocionDesarrolloTransporteFluvial: {
+            nombre: "Dirección de promoción y desarrollo de transporte fluvial de pasajeros"
+          }
+        }
+      }
+
+    }
+  },
+
+  subsecretariaArticulacionInterjurisdiccional: {
+    nombre: "Subsecretaría de articulación interjurisdiccional del transporte",
+    direcciones: {
+      oficinaPrivada: {
+        nombre: "Oficina Privada de Subsecretaría de Articulación Interjurisdiccional"
+      },
+
+      direccionCompensacionesTransporte: {
+        nombre: "Dirección provincial de compensaciones al transporte",
+        direcciones: {
+          direccionCompensacionesTransporte: {
+            nombre: "Oficina privada de la misma dirección"
+          },
+          direccionLiquidacionPago: {
+            nombre: "Dirección de liquidación y pago"
+          },
+          direccionControlVerificacion: {
+            nombre: "Dirección de control y verificación"
+          }
+        }
+      },
+
+      direccionCoordinacionTransporte: {
+        nombre: "Dirección provincial de coordinación del transporte",
+        direcciones: {
+          direccionCoordinacionTransporte: {
+            nombre: "Oficina privada de la misma dirección"
+          },
+          direccionCoordinacionPublico: {
+            nombre: "Dirección de coordinación del transporte con el sector público"
+          },
+          direccionCoordinacionPrivado: {
+            nombre: "Dirección de coordinación del transporte con el sector privado"
+          }
+        }
+      },
+
+      direccionPlanificacionPoliticaEconomica: {
+        nombre: "Dirección provincial de planificación y política económica del transporte",
+        direcciones: {
+          direccionPlanificacionPoliticaEconomica: {
+            nombre: "Oficina privada de la misma dirección"
+          },
+          direccionPlanificacionTransporte: {
+            nombre: "Dirección de planificación del transporte"
+          },
+          direccionAnalisisTransporte: {
+            nombre: "Dirección de análisis eficiente del transporte"
+          },
+          direccionGestionSubsidios: {
+            nombre: "Dirección de gestión de subsidios al transporte"
+          }
+        }
+      },
+
+      direccionPoliticasGeneroInclusivas: {
+        nombre: "Dirección provincial de políticas de género y estrategias inclusivas",
+        direcciones: {
+          direccionPoliticasGeneroInclusivas: {
+            nombre: "Oficina privada de la misma dirección"
+          },
+          direccionAccesibilidadTransporte: {
+            nombre: "Dirección de accesibilidad del transporte"
+          },
+          direccionInclusionDiversidad: {
+            nombre: "Dirección de inclusión y diversidad"
+          }
+        }
+      },
+
+      direccionPoliticasEducativasPrevencion: {
+        nombre: "Dirección provincial de políticas educativas de prevención",
+        direcciones: {
+          direccionPoliticasEducativasPrevencion: {
+            nombre: "Oficina privada de la misma dirección"
+          },
+          direccionPromocionNormasConvivencia: {
+            nombre: "Dirección de promoción de normas de convivencia ciudadana"
+          },
+          direccionGestionInterministerial: {
+            nombre: "Dirección de gestión interministerial de prevención y concientización en el transporte"
+          }
+        }
+      }
+    }
+  },
+  unidadMonitoreo: {
+    nombre: "Unidad de monitoreo y registro general de datos del transporte",
+    oficinaPrivada: {
+      nombre: "Oficina Privada de Unidad de monitoreo y registro general de datos del transporte"
+    }
+  },
+  direccionComunicacion: {
+    nombre: "Dirección de comunicación y relación institucionales",
+    oficinaPrivada: {
+      nombre: "Oficina Privada de Dirección de comunicación y relación institucionales"
+    }
+  }
+};
+
+
